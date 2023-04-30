f5ed1e2bfc18f373d4067e5c1a0cf32e827d02d5
Larisa-Zadesenets <117529302+Larisa-Zadesenets@users.noreply.github.com>
2023-04-30 23:04:45 +0300

    1dz

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList= document.querySelector(".gallery");
+const imgCard = createImgCard (galleryItems);
+galleryList.insertAdjacentHTML("beforeend", imgCard);
+galleryList.addEventListener("click", onImgCardClick);
+
+function createImgCard (galleryItems) {
+    return galleryItems.map(({preview, original, description})=>{
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      
+    />
+  </a>
+</li>
+        `;
+    }).join("");
+}
+
+function onImgCardClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log (evt.target);
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+    `,
+    { onShow: ()=> document.addEventListener("keydown", onCloseModal),
+      onClose: ()=> document.removeEventListener("keydown", onCloseModal)
+    })
+    instance.show()
+
+    function onCloseModal(evt) {
+        if (evt.code ==="Escape") {
+            instance.close();  
+        }
+    }
+}
+  
